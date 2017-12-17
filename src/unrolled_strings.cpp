e0cbed59d6549aa6441c384fe4643d08e37b245d
Sorin Vatasoiu <sorin.vatasoiu@gmail.com>
2017-12-17 16:48:32 -0500

    moved benchmark and improved unroll

--- src/unrolled_strings.cpp
+++ src/unrolled_strings.cpp
@@ -7,18 +7,18 @@ namespace strings
 
 size_t strlen(const char *s) {
     size_t len = 0;
-    for(;;) {
-        uint64_t x = *(uint64_t*)s;
-        if((x & 0xFF) == 0) return len;
-        if((x & 0xFF00) == 0) return len + 1;
-        if((x & 0xFF0000) == 0) return len + 2;
-        if((x & 0xFF000000) == 0) return len + 3;
-        if((x & 0xFF00000000) == 0) return len + 4;
-        if((x & 0xFF0000000000) == 0) return len + 5;
-        if((x & 0xFF000000000000) == 0) return len + 6;
-        if((x & 0xFF00000000000000) == 0) return len + 7;
-        s += 8, len += 8;
+
+    while (!((((*(uint64_t *)s) - 0x0101010101010101ULL) 
+           & ~(*(uint64_t *)s) & 0x8080808080808080ULL)))
+    {
+        s += 8;
+        len += 8;
     }
+
+    while (*s++ && ++len)
+       ;
+
+    return len;
 }
 
 // from https://codereview.stackexchange.com/questions/30337/x86-strcpy-implementation
